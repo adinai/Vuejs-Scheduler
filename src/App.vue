d1f8ec514d17f472d5fc4804511fe65dcfc1d5a9
Adina Kaiymova <adina.kaiymova@gmail.com>
2019-05-22 18:56:46 +0900

    input added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header/>
+    <AddTodo/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   
   </div>
@@ -9,12 +10,15 @@
 <script>
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
 
 export default {
   name: 'app',
   components: {
     Header,
-    Todos
+    Todos,
+    AddTodo
   },
   data() {
      return {
@@ -59,5 +63,18 @@ export default {
 body{
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn{
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover{
+background: #666;
 }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <form>
+        <input type="text" name="title" placeholder="Schedule Your Day...">
+        <input type="submit" value="Add" class="btn"> 
+    </form>
+</div>
+</template>
+
+
+<script>
+export default{
+    name: "AddTodo"
+}
+</script>
+
+
+<style scoped>
+form{
+    display: flex;
+}
+
+input[type="text"]{
+    flex: 10;
+    padding: 5px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+input[type="submit"]{
+    flex: 2;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+</style>
